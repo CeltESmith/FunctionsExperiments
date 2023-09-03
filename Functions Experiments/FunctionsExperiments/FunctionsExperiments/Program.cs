e6f034aae46ada5171e27bb6b5207d1e6b1f4ffb
Elijah Smith <elijah.d.s@outlook.com>
2023-09-02 21:28:55 -0400

    Update to some worked on Material.

--- Functions Experiments/FunctionsExperiments/FunctionsExperiments/Program.cs
+++ Functions Experiments/FunctionsExperiments/FunctionsExperiments/Program.cs
@@ -10,24 +10,14 @@ namespace FunctionsExperiments
     {
         static void Main(string[] args)
         {
-            StringFormatingSystem test1 = new StringFormatingSystem();
-            StringFormatingSystem test2 = new StringFormatingSystem();
+            StringManipulations world= new StringManipulations();
+            StringManipulations dev = new StringManipulations();
 
-            string value=test1.FormatPhoneNumber(StringFormatingSystem.NumFormat.Dashes, "17164329870");
-            Console.WriteLine($"The returned value is {value}");
-            Console.WriteLine();
+            string revWorld=world.ReverseWord("Hello World!");
+            string revDev=dev.ReverseWord("Developer");
 
-            string falseValue = test1.FormatPhoneNumber(StringFormatingSystem.NumFormat.Dashes, "27164329870");
-            Console.WriteLine($"The returned value is {falseValue}");
-            Console.WriteLine();
-
-            string falseValue2 = test1.FormatPhoneNumber(StringFormatingSystem.NumFormat.Dashes, "171649870");
-            Console.WriteLine($"The returned value is {falseValue2}");
-            Console.WriteLine();
-
-            string value2 = test2.FormatPhoneNumber(StringFormatingSystem.NumFormat.Parenthesis, "17164329870");            
-            Console.WriteLine($"The returned value is {value2}");
-            Console.WriteLine();
+            Console.WriteLine($"This is Hello World! back wards:    {revWorld}");
+            Console.WriteLine($"This is Developer back words:      {revDev}");
         }
     }
 }
---
+++ Functions Experiments/FunctionsExperiments/FunctionsExperiments/StringManipulations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsExperiments
+{
+    public class StringManipulations
+    {
+        public string ReverseWord(string word)
+        {
+            char[] reverseChars = word.Reverse().ToArray();
+            string reversedString = "";
+
+            foreach (char c in reverseChars)
+            {
+                reversedString += c;
+            }
+            return reversedString;
+        }
+    }
+}
