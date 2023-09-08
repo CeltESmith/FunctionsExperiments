6f0d00074c4f57852d5c5cd62ad8bed6b00d722e
Elijah Smith <elijah.d.s@outlook.com>
2023-09-08 14:53:42 -0400

    Here is some C# code practice.

---
+++ Functions Experiments/FunctionsExperiments/FunctionsExperiments/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsExperiments
+{
+    public class Calculator
+    {
+        public decimal CalculateMean(decimal[] valueArray)
+        {
+            decimal sumNum = 0;
+            decimal totalValues = 0;
+
+            foreach(var value in valueArray)
+            {
+                sumNum += value;
+                totalValues++;
+            }
+            return sumNum / totalValues;
+        }
+
+    }
+}
