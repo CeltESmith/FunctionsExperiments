72dcb5f2e8c65c83efa02065b419a903ecce119e
Elijah Smith <elijah.d.s@outlook.com>
2023-09-08 14:53:58 -0400

    Here is some C# code practice.

---
+++ Functions Experiments/FunctionsExperiments/FunctionsExperiments/DaysCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsExperiments
+{
+    public class DaysCalculator
+    {
+        public decimal ConvertYearsToDays(decimal year)
+        {
+            return year * 365.25m;
+        }
+    }
+}
